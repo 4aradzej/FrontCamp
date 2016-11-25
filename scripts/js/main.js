72890a504d279d3a8acc60af4adb37ea5c8691fc
Aliaksandr_Rusak <Aliaksandr.Rusak@gmail.com>
2016-11-25 18:05:03 +0300

    added classes

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -14,43 +14,64 @@
         fetch(request, init)
             .then(r => r.json())
             .then((data) => { returnNews(data); })
-            .catch((error) => {viewError({ message : 'something wrog with getting news;' })} );
+            .catch(() => {new NewsError('something wrog with getting news;').view();} );
     }
     
     function returnNews(newsObj){
-        if (newsObj.status == 'ok') {
-            viewArticle(newsObj.articles)
+        let news = new News(newsObj);
+        debugger;
+        if (news.status == 'ok') {
+            let container = document.getElementById("container");
+            while (container.firstChild) {
+                    container.removeChild(container.firstChild);
+                }
+            news.viewArticles(container);
             };
         }
-
-    function viewArticle(objects){
-        let container = document.getElementById("container");
-
-        while (container.firstChild) {
-            container.removeChild(container.firstChild);
+    
+    class NewsError{
+        constructor(e){
+            this.message = e ? e : '';
         }
-       
-        objects.forEach(function(article) {
-            let newNode = document.createElement('div');   
-            let data = getNewsTemplated(article);
+
+        view(){
+            console.log(this.message);
+        }
+
+    }
+
+
+    class News{
+        constructor(data) {
             debugger;
-            newNode.innerHTML = data;
-            container.appendChild(newNode);
-        });
-    }
+            Object.assign(this, data);
+        }
 
-    function getNewsTemplated(article) 
-    {
-        return `<table border='1' width='100%'>
-                    <tr><th><a href=${article.url}>${article.title}</a></th></tr>
-                    <tr><td><image src=${article.urlToImage} />${article.description}</td></tr>
-                    <tr><td>${article.author}</td></tr>
-                </table> `;
-    }
+        viewArticles(elem){
+            this.articles.forEach(function(a){
+                News.viewArticle(elem, a);
+            });
+        }
 
-    function viewError(e)
-    {
-        alert(e.message);
+        static getNewsTemplated(article) {
+            debugger;
+            return `<table border='1' width='100%'>
+                        <tr><th><a href=${article.url}>${article.title}</a></th></tr>
+                        <tr><td><image src=${article.urlToImage} />${article.description}</td></tr>
+                        <tr><td>${article.author}</td></tr>
+                    </table> `;
+        }
+
+        static viewArticle(elem, a) {
+            if (elem)
+            {
+                let newNode = document.createElement('div');   
+                let data = News.getNewsTemplated(a);
+                
+                newNode.innerHTML = data;
+                elem.appendChild(newNode);
+            }
+        }   
     }
 })();  
 
